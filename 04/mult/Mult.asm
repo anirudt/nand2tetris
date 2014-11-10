4d02d98ac2ecd13a8223e33e20c8a1b9c829ae8c
Anirud <anirudt@gmail.com>
2014-11-10 22:21:23 +0530

    Started Assembly\!

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+	@i
+	M=0
+	@R2
+	M=0
+	
+
+(LOOP)
+	@i
+	D=M
+	@R0
+	D=D-M
+	@END
+	D;JGE
+	@R1
+	D=M
+	@R2
+	M=M+D
+	@i
+	M=M+1
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
+
+
+
+ 
---
+++ 04/mult/part.asm
@@ -0,0 +1,23 @@
+//First .asm file written in Hack!
+	@i
+	M=1
+	@sum
+	M=0
+(LOOP)
+	@i
+	D=M
+	@100
+	D=D-A
+	@END
+	D;JGT
+	@sum
+	D=M
+	@i
+	M=D+M
+	@i
+	M=M+1
+	@LOOP
+	0;JMP
+(END)	 
+	@END
+	0;JMP
